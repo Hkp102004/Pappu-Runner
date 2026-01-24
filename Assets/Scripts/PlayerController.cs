50ee43189d5a8ff4b7222d07be2ca18f2a6beec6
HKP102004 <harsh15062004@gmail.com>
2026-01-24 14:29:55 +0530

    feat: make PlayerController script and try to make it debug.log input in console

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    private CharacterController controller;
+    private Vector3 moveInput;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        if (moveInput == null)
+        {
+            Debug.LogError("MoveInput is missing in the PlayerController script");
+            return;
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"The key you pressed is: {moveInput}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
