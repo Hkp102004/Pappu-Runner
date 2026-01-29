7729601ed531d6fbf08c9a9ef98dec04eb0cb2a2
HKP102004 <harsh15062004@gmail.com>
2026-01-29 14:24:29 +0530

    feat: jumping cooldown and coroutine for that

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +7,9 @@ public class playerBehaviour : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float height = 400f;
+    [SerializeField] private float height = 300f;
+    [SerializeField] private bool jump = true;
+    private int count = 0;
     void Start()
     {
         
@@ -29,9 +32,27 @@ public class playerBehaviour : MonoBehaviour
             transform.position = new Vector3(-9.3f, transform.position.y, transform.position.z);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)) //jumpting 
+        {
+            Jump();
+        }
+    }
+
+    public void Jump() //jump function 
+    {
+        if(count < 2)
         {
             transform.Translate(Vector3.up * height * Time.deltaTime);
+            count++;
+            Debug.Log(count);
+            StartCoroutine(JumpCooldown()); //cooldown for jump
         }
+    }
+
+    IEnumerator JumpCooldown() //cooldown for jump to wait 2 seconds
+    {
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Couroutine ended");
+        count = 0;
     }
 }
