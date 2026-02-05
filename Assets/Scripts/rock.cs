3ccdce64328f419310cbc99a4cecf412bf224168
HKP102004 <harsh15062004@gmail.com>
2026-02-05 14:33:39 +0530

    feat: make rock script and boulder moving logic

--- Assets/Scripts/rock.cs
+++ Assets/Scripts/rock.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class rock : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 15f;
     void Start()
     {
         
@@ -11,6 +12,6 @@ public class rock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
