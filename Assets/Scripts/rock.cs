f30ca9176f3a0bdea202b9f4d61b9f9b39b0d6e3
HKP102004 <harsh15062004@gmail.com>
2026-02-05 14:37:50 +0530

    feat: make boulder destroy logic on going offset

--- Assets/Scripts/rock.cs
+++ Assets/Scripts/rock.cs
@@ -1,17 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class rock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]private Transform player;
     [SerializeField] private float speed = 15f;
+    [SerializeField] private float deathzone = 18f;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if(player == null)
+        {
+            Debug.LogError("Tranform of player is missing in rock script");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if(transform.position.x <= player.position.x - deathzone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
