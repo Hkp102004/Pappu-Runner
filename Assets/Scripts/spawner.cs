38270f2ab6bb6dae2425e6e1ea51e00e8bd3cb27
HKP102004 <harsh15062004@gmail.com>
2026-02-05 19:08:55 +0530

    feat: make spawner position relative to player

--- Assets/Scripts/spawner.cs
+++ Assets/Scripts/spawner.cs
@@ -5,15 +5,27 @@ public class spawner : MonoBehaviour
 {
     [SerializeField] private GameObject rock; //the rock prefab that should be sopwaned again and again
     [SerializeField] private float spawnRate = 2f; //the rate at which the bopulders should be spawned
+    [SerializeField] private Transform player_position;
 
     void Start()
     {
-        
+        if(rock == null)
+        {
+            Debug.LogError("rock is missing in the spawner script");
+            return;
+        }
+        if(player_position == null)
+        {
+            Debug.LogError("Player position is missing in the spawner script");
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(player_position.position.x + 17, transform.position.y, transform.position.z);
+
         if(spawnRate <= 0)
         {
             Instantiate(rock, new Vector3(transform.position.x, Random.Range(-0.9f,5f), transform.position.z), Quaternion.identity);
