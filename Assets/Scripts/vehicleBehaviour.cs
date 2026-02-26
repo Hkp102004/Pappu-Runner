afe5dcd7aee76f34b37a2d5a9c01f825ef05d167
Hekey <87144005+Hkp102004@users.noreply.github.com>
2026-02-26 16:21:29 +0530

    feat: make player_position varible in VehicleBehaviour script

--- Assets/Scripts/vehicleBehaviour.cs
+++ Assets/Scripts/vehicleBehaviour.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class vehicleBehaviour : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    Transform player_position;
     void Start()
     {
-        
+        player_position = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if(player_position == null)
+        {
+            Debug.LogError("player_position is missing from ", gameObject);
+            return;
+        }
     }
 
     // Update is called once per frame
