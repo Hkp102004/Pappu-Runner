bfcb7cc7c2384f69b9606cc0cba36786ca37641b
Hekey <87144005+Hkp102004@users.noreply.github.com>
2026-03-11 21:44:11 +0530

    feat: make cloud behavior and deletion logic
    made a cloud that moves at a very slow speed and get's deleted after it moves away from the player

---
+++ Assets/Scripts/cloudBehaviour.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class cloudBehaviour : MonoBehaviour
+{
+    private float deathzone = 15;
+    private float speed = 1.5f;
+    Transform player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        if(player == null)
+        {
+            Debug.LogError("the player position is missing from the cloud script");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement(); //the movement of the cloud
+        deletion(); //the condition check and the deletion of the cloud
+    }
+
+    void movement()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    void deletion()
+    {
+        if(transform.position.x <= player.position.x - deathzone)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
